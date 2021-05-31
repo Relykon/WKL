277e8486d480442247360839d25eabccdb9b5203
Kyle Harrington <harrington.kyle@gmail.com>
2021-05-31 15:40:50 -0700

    add materialUI to package, create Standings component table

--- wkl-app/src/router.js
+++ wkl-app/src/router.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Home from './components/Home/Home.jsx'
-import Rules from './components/Rules/Rules.jsx'
-import LeagueCreate from './components/LeagueCreate/LeagueCreate.jsx'
-// import Home from './components/Home/Home.jsx'
+import Home from './components/Home.jsx'
+import Rules from './components/Rules.jsx'
+import LeagueCreate from './components/Fantasy/LeagueCreate.jsx'
+import FantasyDashboard from './components/Fantasy/FantasyDashboard.jsx'
 
 const routes = {
   "/": () => <Home />,
-  "/Rules": () => <Rules />,
-  "/LeagueCreate": () => <LeagueCreate />
-
+  "/fantasy": () => <FantasyDashboard />,
+  "/rules": () => <Rules />,
+  "/leagueCreate": () => <LeagueCreate />
 };
 
 export default routes;
