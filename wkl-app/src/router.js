c2d6c15c3f9e70dcb93917aba06a6176667f8785
Kyle Harrington <harrington.kyle@gmail.com>
2021-05-31 20:21:35 -0700

    add scorekeeper tabs to organize scorekeeper views. update structure to utilize hookrouter properly

--- wkl-app/src/router.js
+++ wkl-app/src/router.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Home from './components/Home.jsx'
-import Rules from './components/Rules.jsx'
-import LeagueCreate from './components/Fantasy/LeagueCreate.jsx'
-import FantasyDashboard from './components/Fantasy/FantasyDashboard.jsx'
+import ScorekeeperDashboard from './components/Scorekeeper/ScorekeeperDashboard.jsx';
+import FantasyDashboard from './components/Fantasy/FantasyDashboard.jsx';
 
 const routes = {
   "/": () => <Home />,
   "/fantasy": () => <FantasyDashboard />,
-  "/rules": () => <Rules />,
-  "/leagueCreate": () => <LeagueCreate />
+  "/scorekeeper": () => <ScorekeeperDashboard />,
 };
 
 export default routes;
