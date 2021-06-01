a700048372e9c45eaf32117fc50b238ea7460b51
Kyle Harrington <harrington.kyle@gmail.com>
2021-05-31 20:21:35 -0700

    add scorekeeper tabs to organize scorekeeper views. update structure to utilize hookrouter properly

--- wkl-app/src/components/Home.jsx
+++ wkl-app/src/components/Home.jsx
@@ -1,22 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import NavBar from './NavBar.jsx';
-import Standings from './Standings.jsx'
-// import FantasyDashboard from './FantasyDashboard';
-
-import logo from '../assets/img/Logo.png';
+import React from 'react';
+import NavBar from './NavBar'
+import Standings from './Standings'
 
 
 const Home = () => {
 
   return (
-    <div className="app" style={{backgroundImage: `url(${logo})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat'}}>
-      {/* <NavBar /> */}
+    <div>
       <h1>HOME PAGE!</h1>
-      <NavBar />
       <Standings />
     </div>
   );
-
 };
 
 export default Home;
--- wkl-app/src/components/NavBar.jsx
+++ wkl-app/src/components/NavBar.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { A } from 'hookrouter';
-
+import logo from '../assets/img/Logo.png';
 
 const NavBar = () => {
-
   return (
-    <div>
-      <A href='/fantasy'> Fantasy Kickball </A> <br/>
-      <A href='/rules'> "Rules" of Kickball </A>
+    <div className="nav-bar" >
+      <A href='/fantasy'> Fantasy Kickball </A>
+      <A href='/'> <img src={logo} alt="WKL logo" style={{height: '2em'}} /> </A>
+      <A href='/scorekeeper'> Scorekeeper </A>
     </div>
   );
 };
